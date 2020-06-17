b37cdbcf20413fa062d2049620181ccdd330f27a
Matheus Brodt <matheus_brodt@outlook.com>
2020-06-17 01:13:51 -0300

    System

--- app principal.py
+++ app principal.py
@@ -1,35 +1,47 @@
+# controle de estoque de lentes
 
-try:
-    while True:
-        print("""        [1] para orgânica
-        [2] para poly
-        [3] para orgânica blue cut
-        [4] para poly blue cut
-        [5] para alto índice
-        [6] para zeiss blue protect
-        [7] para zeiss silver
-        [8] para zeiss platinum
-        [9] para reiniciar""")
-        material = int(input('Digite a opção: '))
-        if material == 1:
-            print('Add em orgânica')
-        elif material == 2:
-            print('Add em poly')
-        elif material == 3:
-            print('Add em organica blue cut')
-        elif material == 4:
-            print('Add em poly blue cut')
-        elif material == 5:
-            print('Add em alto índice')
-        elif material == 5:
-            print('Add em zeiss blue cut')
-        elif material == 6:
-            print('Add em zeiss silver')
-        elif material == 7:
-            print('Add em zeiss silver')
-        elif material == 8:
-            print('Add em zeiss platinum')
-        else:
-            print('Saindo do sistema')
-except TypeError:
-    print('Erro!')
+print("""[1] para cadastrar lente.
+[2] para visualizar estoque.""")
+option = int(input('Digite uma opção: '))
+if option == 1:
+    print('Option one')
+    try:
+        while True:
+            origin = int(input('Filhal: '))  # Identifies the branch you are adding to inventory
+            #  Menu
+            print("""        [1] para orgânica
+            [2] para poly
+            [3] para orgânica blue cut
+            [4] para poly blue cut
+            [5] para alto índice
+            [6] para zeiss blue protect
+            [7] para zeiss silver
+            [8] para zeiss platinum
+            [9] para reiniciar""")
+            material = int(input('Digite a opção: '))  # material option
+            if material == 1:
+                print('Add em orgânica')
+            elif material == 2:
+                print('Add em poly')
+            elif material == 3:
+                print('Add em organica blue cut')
+            elif material == 4:
+                print('Add em poly blue cut')
+            elif material == 5:
+                print('Add em alto índice')
+            elif material == 5:
+                print('Add em zeiss blue cut')
+            elif material == 6:
+                print('Add em zeiss silver')
+            elif material == 7:
+                print('Add em zeiss silver')
+            elif material == 8:
+                print('Add em zeiss platinum')
+            else:
+                print('Saindo do sistema')
+    except (TypeError, ValueError):
+        print('\033[31mErro ao cadastrar uma lente!\033[m')
+elif option == 2:
+    print('Option 2')
+else:
+    print('Entrada inválida!')
--- system/lens/__init__.py
+++ system/lens/__init__.py
@@ -1,22 +1,6 @@
-def lens_register(type, spherical_diopter=0, lylinder_diopter=0):
-    material = type
-    if material == 1:
-        print('Add em orgânica')
-    elif material == 2:
-        print('Add em poly')
-    elif material == 3:
-        print('Add em organica blue cut')
-    elif material == 4:
-        print('Add em poly blue cut')
-    elif material == 5:
-        print('Add em alto índice')
-    elif material == 5:
-        print('Add em zeiss blue cut')
-    elif material == 6:
-        print('Add em zeiss silver')
-    elif material == 7:
-        print('Add em zeiss silver')
-    elif material == 8:
-        print('Add em zeiss platinum')
+def read_whole(txt):
+    option = txt
+    if option.isalpha():
+        print('Digite o número da opção: ')
     else:
-        print('Saindo do sistema')
+        return int(option)
