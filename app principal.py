009d5ee614d66f5155d94a902a6255e5b122e3fe
Matheus Brodt <matheus_brodt@outlook.com>
2021-01-24 02:05:26 -0300

    commit dia 24/01/21

--- app principal.py
+++ app principal.py
@@ -1,22 +1,32 @@
 # controle de estoque de lentes
 
 from system.lens import *
-lens = {'1': '1.56_ar', '2': '1.56_blue', '3': 'poly_ar', '4': '1.67', '5': 'zeiss_blue', '6': 'zeiss_platinum',
-        '7': 'zeiss_silver', '8': 'zeiss_photo'}
-laboratory = {'1': 'haytek', '2': 'zeiss'}
 
 try:
     while True:
         print("""    [1] para cadastrar lente.
     [2] para visualizar estoque.
     [3] para exibir as lentes que não tem em estoque.
-    [4] para sair.""")
+    [4] para retirar uma lente.
+    [5] para sair.""")
         option = read_whole('Digite uma opção: ')
-        if option == 1:
-            while True:
+        if option == 1:  # para registrar uma lente
+            record_type = read_whole('[1] para adicionar pelo código de barras.\n'
+                                     '[2] para adicionar pela dioptria.\n Digite: \n')
+            if record_type == 1: # adicionar atraves do codigo de barras
+                while True:
+                    register_cod('Digite o código de barras: ', 'Digite a quantidade: ')
+                    opt = int(input('[1] Para cotinuar. [2] Para sair.'))
+                    if opt == 1:
+                        continue
+                    elif opt == 2:  # adicionar atraves da dioptria e do material da lente
+                        break
+                    else:
+                        print('Opção Inválida')
+            elif record_type == 2:
                 menu()
-                register('Selecione o labortório: ', 'Selecione o material: ', 'Digite a dioptria esferica: ',
-                         'Digite a dioptria cilindrica: ', 'Digite a quantidade: ')
+                register_diopter('Selecione o laboratório: ', 'Selecione o material: ', 'Dioptria esférica: ',
+                                 'Dioptria cilindrica: ', 'Digite a quantidade: ')
                 opt = int(input('[1] Para cotinuar. [2] Para sair.'))
                 if opt == 1:
                     continue
@@ -24,32 +34,47 @@ try:
                     break
                 else:
                     print('Opção Inválida')
-        elif option == 2:
+            else:
+                print('\033[33mOpção Inváida!\033[m')
+        elif option == 2:  # para pesquisar uma lente
             while True:
                 menu()
                 search('Selecione o laboratório: ', 'Selecione o material: ', 'Digite a dioptria esférica: ',
                        'Digite a dioptria cilindríca: ')
-                pesq_option = read_whole('Continuar pesquisando 1 > S / 2 > N: ')
-                if pesq_option == 1:
+                opt = read_whole('Continuar pesquisando 1 > S / 2 > N: ')
+                if opt == 1:
                     continue
-                elif pesq_option == 2:
+                elif opt == 2:
                     break
                 else:
                     print('\033[31mOpção Inválida!\033[m')
                     continue
-        elif option == 3:
+        elif option == 3:  # para exibir as lentes em falta do estoque
             while True:
                 menu()
                 zero('Selecione o laboratório: ', 'Selecione o material: ')
-                pesq_option = read_whole('Continuar pesquisando 1 > S / 2 > N: ')
-                if pesq_option == 1:
+                opt = read_whole('Continuar pesquisando 1 > S / 2 > N: ')
+                if opt == 1:
                     continue
-                elif pesq_option == 2:
+                elif opt == 2:
                     break
                 else:
                     print('\033[31mOpção Inválida!\033[m')
                     continue
-        elif option == 4:
+        elif option == 4:  # para retirar uma lente do estoque
+            while True:
+                menu()
+                exit_stock('Selecione o laboratório: ', 'Selecione o material: ', 'Digite a dioptria esférica: ',
+                           'Digite a dioptria cilindrica: ', 'Digite a quantidade: ')
+                opt = read_whole('Continuar retirando 1 > S / 2 > N: ')
+                if opt == 1:
+                    continue
+                elif opt == 2:
+                    break
+                else:
+                    print('\033[31mOpção Inválida!\033[m')
+                    continue
+        elif option == 5:
             print('\033[33mFinalizando o sistema!\033[m')
             break
         else:
--- system/lens/__init__.py
+++ system/lens/__init__.py
@@ -39,45 +39,27 @@ def search(lab, material, esf_diopter, cil_diopter):
                        f" AND laboratory = '{laboratory[f'{lab_1}']}'")
         result = cursor.fetchone()
         for r in result:
-            print(f'\033[34mTem {r} unidade(s) no estoque.\033[m')
+            print(f"\033[1:31:40m|{c_diopeter}|{lens[f'{material_1}']}|{r} unidades|\033[m")
         conection.close()
         return True
     except:
         print('Lente não localizada no estoque!')
         return False
 
-def register(lab, material, esf_diopter, cil_diopter, amount):
-    """
-    Function for lens register in the database.
-    :param lab: Laboratory.
-    :param material: Lens material.
-    :param esf_diopter: spherical diopter.
-    :param cil_diopter: cylindrical diopter.
-    :param amount: Quantities added.
-    :return: Register lens in the database.
-    """
+
+def register_cod(cod, amount):
     import mysql.connector
-    from datetime import date
-    lens = {'1': '1.56_ar', '2': '1.56_blue', '3': 'poly_ar', '4': '1.67', '5': 'zeiss_blue', '6': 'zeiss_platinum',
-            '7': 'zeiss_silver', '8': 'zeiss_photo'}
-    laboratory = {'1': 'haytek', '2': 'zeiss'}
-    data = date.today()
     try:
-        lab_1 = read_whole(lab)
-        material_1 = read_whole(material)
-        espheric = str(input(esf_diopter))
-        cilinder = str(input(cil_diopter))
-        c_diopter = f'{espheric}/-{cilinder}'
+        bar_code = read_whole(cod)
         amount_1 = read_whole(amount)
         conection = mysql.connector.connect(host='localhost', user='root', password='', database='lab_carol')
         cursor = conection.cursor()
-        cursor.execute(f"INSERT IGNORE INTO stock "
-                       f"VALUES ('{c_diopter}', '{lens[f'{material}']}', '{laboratory[f'{lab}']}', "
-                       f"'{amount_1}', '{data}')")
-        conection.close()
+        cursor.execute(f"UPDATE stock SET amount = amount+{amount_1} WHERE cod_barras = {bar_code}")
         conection.commit()
+        print('\033[34mLente cadastrada com sucesso!\033[m')
     except:
         print('\033[31mErro ao cadastrar a lente!\033[m')
+
 
 def menu():
     lens = {'1': '1.56_ar', '2': '1.56_blue', '3': 'poly_ar', '4': '1.67', '5': 'zeiss_blue', '6': 'zeiss_platinum',
@@ -96,6 +78,7 @@ def menu():
           f'7 > {lens["7"]}\n'
           f'8 > {lens["8"]}\n')
 
+
 def zero(lab, material):
     """
     Lens search function does not exist in the database.
@@ -104,21 +87,117 @@ def zero(lab, material):
     :return: The lents you don't have in the database.
     """
     import mysql.connector
-    lens = {'1': '1.56_ar', '2': '1.56_blue', '3': 'poly_ar', '4': '1.67', '5': 'zeiss_blue', '6': 'zeiss_platinum',
-            '7': 'zeiss_silver', '8': 'zeiss_photo'}
+    lens = {'1': 'Lente Vis Simples 1.50 c/A.R.', '2': 'Lente Vis Simples 1.56 c/A.R.',
+            '3': 'Lente V.S. 1.56 Filtro Azul c/A.R.', '4': 'Lente Ac. Progressiva 1.56 c/A.R.',
+            '5': 'Lente Vis Simp 1.59 Poly c/A.R.', '6': 'zeiss_platinum',
+            '7': 'zeiss_silver', '8': 'zeiss_photo', '9': 'zeiss_blue'}
     laboratory = {'1': 'haytek', '2': 'zeiss'}
     lab_1 = read_whole(lab)
-    material_1 = read_whole(material)
+    material_1 = read_material(material)
     try:
         conection = mysql.connector.connect(host='localhost', user='root', password='', database='lab_carol')
         cursor = conection.cursor()
-        cursor.execute(f"SELECT diopter, data FROM stock "
-                       f"WHERE material = '{lens[f'{material_1}']}' AND laboratory = '{laboratory [f'{lab_1}']}'"
+        cursor.execute(f"SELECT spherical, cylindrical FROM stock "
+                       f"WHERE material = '{lens[f'{material_1}']}' AND laboratory = '{laboratory[f'{lab_1}']}'"
                        f" AND amount = '0'")
         result = cursor.fetchall()
-        print(f'\033[33mLentes com \033[31m0\033[m \033[33munidades em estoque.\033[m')
+        print(f'\033[34mLentes {lens[f"{material_1}"]} em falta:\033[m')
         for r in result:
-            print(f'Dioptria = {r[0]} - DATA = {r[1]}')
+            sphe = (r[0])
+            cyl = (r[0+1])
+            print(f'|ESFÉRICO: \033[31m{sphe:.2f}\033[m| CILINDRO: \033[31m{cyl:.2f}\033[m|')
         cursor.close()
     except:
         print(f'\033[31mErro ao pesquisar grade zerada no estoque!\033[m')
+
+
+def exit_stock(lab, material, esf_diopter, cil_diopter, amount):
+    import mysql.connector
+    lens = {'1': '1.56_ar', '2': '1.56_blue', '3': 'poly_ar', '4': '1.67', '5': 'zeiss_blue', '6': 'zeiss_platinum',
+            '7': 'zeiss_silver', '8': 'zeiss_photo'}
+    laboratory = {'1': 'haytek', '2': 'zeiss'}
+    lab_1 = read_lab(lab)
+    mat = read_whole(material)
+    espherical = str(input(esf_diopter))
+    cylindrical = str(input(cil_diopter))
+    c_diopter = f'{espherical}/-{cylindrical}'
+    amount_1 = read_whole(amount)
+    try:
+        conection = mysql.connector.connect(host='localhost', user='root', password='', database='lab_carol')
+        cursor = conection.cursor()
+        cursor.execute(f"UPDATE stock SET amount = amount-{amount_1} WHERE diopter = '{c_diopter}' "
+                       f"AND material = '{lens[f'{mat}']}' AND laboratory = '{laboratory[f'{lab_1}']}'")
+        cursor.close()
+        conection.commit()
+    except:
+        print('\033[31mErro ao retirar a lente do estoque!\033[m')
+
+
+def read_lab(txt):
+    loop = False
+    while not loop:
+        try:
+            var = str(input(txt))
+            if var == str(1) or var == str(2):
+                loop = True
+                return int(var)
+            else:
+                print('\033[31mErro ao selecionar o laboratório!\033[m')
+                continue
+        except(ValueError, TypeError):
+            print('\033[31mErro ao selecionar o laboratório!\033[m')
+
+
+def read_material(txt):
+    validate_1 = read_whole(txt)
+    try:
+        loop = False
+        while not loop:
+            validate = int(validate_1)
+            if validate >= 1 or validate <= 8:
+                loop = True
+                return int(validate)
+            else:
+                print('\033[31mErro ao selecionar o material!\033[m')
+                continue
+    except(ValueError, TypeError):
+        print('\033[31mErro ao selecionar o material!!\033[m')
+
+
+def read_diopter(txt):
+    try:
+        loop = False
+        while not loop:
+            validate = str(input(txt)).replace(',', '.')
+            if validate.isalpha() or validate.isspace():
+                print('\033[31mDigite a dioptria de forma correta!\033[m')
+                continue
+            else:
+                loop = True
+                return float(validate)
+    except (ValueError, TypeError):
+        print('\033[31mDigite a dioptria de forma correta!\033[m')
+
+
+def register_diopter(lab, material, sphe_diopter, cyl_diopter, amount):
+    import mysql.connector
+    lens = {'1': 'Lente Vis Simples 1.50 c/A.R.', '2': 'Lente Vis Simples 1.56 c/A.R.',
+            '3': 'Lente V.S. 1.56 Filtro Azul c/A.R.', '4': 'Lente Ac. Progressiva 1.56 c/A.R.',
+            '5': 'Lente Vis Simp 1.59 Poly c/A.R.', '6': 'zeiss_platinum',
+            '7': 'zeiss_silver', '8': 'zeiss_photo', '9':'zeiss_blue'}
+    laboratory = {'1': 'haytek', '2': 'zeiss'}
+    try:
+        lab_1 = read_lab(lab)
+        material_1 = read_material(material)
+        spherical = read_diopter(sphe_diopter)
+        cylinder = read_diopter(cyl_diopter)
+        amount_1 = read_whole(amount)
+        conection = mysql.connector.connect(host='localhost', user='root', password='', database='lab_carol')
+        cursor = conection.cursor()
+        cursor.execute(f"UPDATE stock SET amount = amount+{amount_1} WHERE laboratory = '{laboratory[f'{lab_1}']}' "
+                       f"AND spherical = {spherical} AND cylindrical = {cylinder} AND "
+                       f"material = '{lens[f'{material_1}']}'")
+        conection.commit()
+        print('\033[33mLente cadastrada com sucesso!\033[m')
+    except:
+        print('\033[31mErro ao cadastrar a lente!\033[m')
