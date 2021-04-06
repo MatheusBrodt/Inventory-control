af617815be26e723c13ec54ff80b12faf07c9c0f
MatheusBrodt <matheus_brodt@outlook.com>
2021-04-06 00:00:14 -0300

    Commit dia 05/04 Utilizavel as funções de cadastro de lentes e de retirada de lentes indicando seus destinos;

--- Interface.py
+++ Interface.py
@@ -7,6 +7,10 @@ root = Tk()
 
 ### BACKEND ###
 class Funcs():
+    def date_Today(self):
+        from datetime import date
+        self.date = date.today()
+
     def connect_BD(self):
         try:
             self.conn = mysql.connector.connect(host='localhost', user='root', password='', database="lab_carol")
@@ -24,6 +28,12 @@ class Funcs():
         self.lab = self.lab_Entry.delete(0, END)
         self.mat_Capt = self.mat_Entry.delete(0, END)
 
+    def clear_Exit(self):
+        self.codRemove_Capt = self.codBarrasEntry_Exit.delete(0, END)
+        self.store_Capt = self.storeEntry_Exit.delete(0, END)
+        self.seq_Capt = self.seqEntry_Exit.delete(0, END)
+        self.reason_Capt = self.reasonEntry_Exit.delete(0, END)
+
     def captura_dados(self):
         self.codigo_Capt = self.codBarrasEntry.get()
         self.sphe_Capt = self.sphe_Entry.get()
@@ -33,15 +43,38 @@ class Funcs():
         self.lab_Capt = self.lab_Entry.get()
         self.mat_Capt = self.mat_Entry.get()
 
-    def verification_Int(self):  #>>> VERIFICAR ESTA FUNÇÃO
+    def captura_dadosRemove(self):
+        self.codRemove_Capt = self.codBarrasEntry_Exit.get()
+        self.store_Capt = self.storeEntry_Exit.get()
+        self.seq_Capt = self.seqEntry_Exit.get()
+        self.reason_Capt = self.reasonEntry_Exit.get()
+
+    def verification_Int(self):
         self.captura_dados()
         try:
             validate = str(self.codigo_Capt)
             if validate.isalpha() or validate.isspace():
                 self.text_warning = 'DADOS INVÁLIDOS'
-                self.sound()
+                self.warning()
                 self.clear()
-                print('\033[31mEntrada Inválida do if\033[m')
+                print('\033[31mENTRADA DE DADOS INVALIDA NA FUNÇÃO "VERIFICATON_INT"\033[m')
+                return False
+            else:
+                return True
+                self.text_warning = ''
+                print('Dados aceitos!')
+        except (ValueError, TypeError):
+            print('\033[31mEntrada Inválida do try\033[m')
+
+    def verification_IntExit(self):
+        self.captura_dadosRemove()
+        try:
+            validate = str(self.codRemove_Capt)
+            if validate.isalpha() or validate.isspace():
+                self.text_warning = 'DADOS INVÁLIDOS'
+                self.warning()
+                #self.clear() CRIAR FUNÇÃO CLEAR DA SAÍDA DE LENTES
+                print('\033[31mENTRADA DE DADOS INVALIDA NA FUNÇÃO "VERIFICATON_INT"\033[m')
                 return False
             else:
                 return True
@@ -53,16 +86,35 @@ class Funcs():
     def verification_Code(self):
         self.captura_dados()
         self.connect_BD()
-        self.cursor.execute(f"SELECT amount FROM stock WHERE cod_barras = {self.codigo_Capt}")
-        result = self.cursor.fetchone()
-        if result is None:
-            result = 0
+
+        self.codigos = []
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(f"SELECT cod_barras FROM stock ")
+        result = self.cursor.fetchall()
+        if len(self.codigo_Capt) < 10:
+            self.text_warning = 'C. BARRAS MÍNIMO 10 DÍGITOS'
+            self.warning()
+            print('CÓD. BARRAS MÍNIMO 10 DÍGITOS')
         else:
-            if result[0] > int(0):
-                print('\033[31mChamando a função "Register_Cod"!\033[m')
-                self.register_cod()
+            for d in result:
+                e = d
+                for c in e:
+                    self.codigos.append(c)
+
+            if int(self.codigo_Capt) in self.codigos:
+                self.cursor.execute(f"SELECT amount FROM stock WHERE cod_barras = {self.codigo_Capt}")
+                result = self.cursor.fetchone()
+                if result is None:
+                    result = 0
+                else:
+                    if result[0] >= int(0):
+                        print('\033[31mChamando a função "Register_Cod"!\033[m')
+                        self.register_cod()
             else:
+                print('Não existe esta lente na base de dados! VERIFICATION CODE'.title())
                 self.text_warning = 'CADASTRAR LENTE'
+                self.warning()
+                self.register_cod()
 
     def insert_Dados(self, event):
         self.connect_BD()
@@ -83,39 +135,47 @@ class Funcs():
         self.eye_Entry.insert(END, eye)
         self.lab_Entry.insert(END, lab)
 
-    # ESTA FUNÇÃO CASO JA TENHA O CÓDIGO DE BARRAS CADASTRADO
     def register_cod(self):
-        if self.verification_Int():
-            self.captura_dados()
-            codigos = []
-
-            self.cursor = self.conn.cursor()
-            self.cursor.execute(f"SELECT cod_barras FROM stock ")
-            result = self.cursor.fetchall()
-            for d in result:
-                e = d
-                for c in e:
-                    codigos.append(c)
-            if int(self.codigo_Capt) in codigos:
+        try:
+            if int(self.codigo_Capt) in self.codigos:  # SE JA EXISTIR O CÓDIGO CADASTRADO
                 self.insert_Dados(event='')
                 self.cursor.execute(f"UPDATE stock SET amount = amount+{1} WHERE cod_barras = {self.codigo_Capt}")
                 self.conn.commit()
                 self.conn.close()
                 self.text_warning = 'LENTE ADICIONADA'
                 self.warning()
+                self.clear()
                 print(f'\033[34mLente adicionada com sucesso!\033[m'.title())
             else:
-                print('Não existe esta lente na base de dados!'.title()) #>>> CRIAR FUNÇÃO PARA CADASTRAR A LENTE
+                print('Não existe esta lente na base de dados! REGISTER COD'.title())
                 self.text_warning = 'CADASTRAR LENTE'
-                self.sound()
-                self.clear()
-                self.connect_BD()
-                self.captura_dados()
-                self.cursor.execute(f"INSERT INTO stock VALUES "
-                                    f"('{self.codigo_Capt}', '{self.mat_Capt}', '{self.sphe_Capt}', "
-                                    f"'{self.cylin_Capt}', '{self.add_Capt}', '{self.eye_Capt}', "
-                                    f"'{self.lab_Capt}', '1' )")
-                print('\033[31mCadastrada lente ixesistete!\033[m'.title())
+                self.warning()
+                try:
+                    self.captura_dados()
+                    if self.mat_Capt != '':  # SE NÃO EXISTIR O CÓDIGO CADASTRADO
+                        print('\033[31mEntrando no cadastro de lentes após o campo material ter sido preenchido!\033[m')
+                        self.label_and_entry(); print('Chamando "LABEL AND ENTRY"')
+                        self.connect_BD()
+                        self.cursor.execute(f"INSERT INTO stock VALUES "
+                                            f"('{self.codigo_Capt}', '{self.mat_Capt}', '{self.sphe_Capt}', "
+                                            f"'{self.cylin_Capt}', '{self.add_Capt}', '{self.eye_Capt}', "
+                                            f"'{self.lab_Capt}', '1' )")
+                        self.text_warning = 'LENTE CADASTRADA'
+                        self.warning()
+                        self.clear()
+                        print('\033[31mCadastrada lente ixesistete!\033[m'.title())
+                    else:
+                        self.text_warning = 'DIGITE O MATERIAL'
+                        self.warning()
+                        print('CAMPO MATERIAL VAZIO')
+                except:
+                    self.text_warning = 'ERRO AO INSERIR DADOS'
+                    self.warning()
+                    print('ERRO AO INSERIR DADOS 1')
+        except:
+            self.text_warning = 'ERRO AO INSERIR DADOS'
+            self.warning()
+            print('ERRO AO INICIAR REGISTER COD')
 
     def warning(self):
         self.font_warning = ('Verdana', 20, 'italic', 'bold')
@@ -123,13 +183,6 @@ class Funcs():
                                         bg='#f0e68c', fg='red')
 
         self.text_warning_Label.place(relx=0.05, rely=0.60, relwidth=0.90, relheight=0.16)
-
-    def sound(self):
-        #  REPRODUZ TRÊS BIPS EM SEGUIDA
-        import winsound
-        for c in range(3):
-            b = winsound.Beep(332, 200)
-            self.warning()
 
     def label_and_entry(self):
         self.options()
@@ -169,7 +222,6 @@ class Funcs():
         self.mat_Entry.place(relx=0.665, rely=0.295, relwidth=0.30, relheight=0.11)
 
 
-    ### ENTRADA DE DADOS ###
 ### FRONT END ###
 class Interface(Funcs):
     def __init__(self):
@@ -189,6 +241,7 @@ class Interface(Funcs):
         self.entry_Login()
         self.entry_Senha()
         self.logo()
+        self.date_Today()
         root.mainloop()
 
     # FRAMES
@@ -226,22 +279,35 @@ class Interface(Funcs):
 
 
     ### BOTÕES ###
-
     #>>>>>
-    # OPÇÕES DO BOTÃO ENTER   ###>>>> ARRUMAR AQUI <<<<### O COMANDO DO MYSQL
+    # OPÇÕES DO BOTÃO ENTER
     def option_Ir(self):
         print("Botão 'ir' clicado!".title())
         self.verification_Code()
         self.captura_dados()
-        self.clear()
-        print('Opção do botão IR concluída!')
 
     #  BOTÃO ENTER DE CADASTRO DE LENTES
     def button_Ir(self):
         self.fontepadrao = ("Verdana", 10, "italic", 'bold')
-        self.Ir = Button(self.frame_options, text='Ir', font=self.fontepadrao, bg='#f0e68c', command=self.option_Ir)
+        self.Ir = Button(self.frame_options, text='Cadastrar', font=self.fontepadrao, bg='#f0e68c',
+                         command=self.option_Ir)
 
         self.Ir.place(relx=0.425, rely=0.80, relwidth=0.15, relheight=0.15)
+    #<<<<<
+
+    #>>>>>
+    # OPÇÕES DO BOTÃO ENTER PARA RETIRAR LENTES
+    def option_ButtonExit(self):
+        print("Botão 'ENTER/RETIRAR' clicado!")
+        self.remove_LensAdd()
+
+    #  BOTÃO ENTER DE RETIRADA DE LENTES
+    def button_Exit(self):
+        self.fontepadrao = ("Verdana", 10, "italic", 'bold')
+        self.exitButton = Button(self.frame_options, text='Retirar', font=self.fontepadrao, bg='#f0e68c',
+                         command=self.option_ButtonExit)
+
+        self.exitButton.place(relx=0.425, rely=0.80, relwidth=0.15, relheight=0.15)
     #<<<<<
 
     #>>>>>
@@ -262,7 +328,6 @@ class Interface(Funcs):
     def exibirOpcoes(self):
         print("Botão visualizar estoque clicado!".title())
         self.label_and_entry()
-
     #  BOTÃO VISUALIZAR
     def button_VisEstoque(self):
         self.VisEstoque = Button(self.frame_buttons, text="Vis. Estoque", bd=2, bg="#c0c0c0", fg="black",
@@ -276,7 +341,6 @@ class Interface(Funcs):
     def zero_lens(self):
         print("Botão lentes em falta clicado!".title())
         self.options()
-
     # BOTÃO LENTES ZERADAS
     def button_LensZero(self):
         self.LensZero = Button(self.frame_buttons, text="Lentes\nem\nFalta", bd=2, bg="#c0c0c0", fg="black",
@@ -290,11 +354,28 @@ class Interface(Funcs):
     def lens_output(self):
         print("Botão retirar lente clicado!".title())
         self.options()
+        self.button_Exit()
         self.fontepadrao = ("Verdana", 10, "italic", 'bold')
+        # LABELS  >>>>  CÓDIGO, SEQ, MOTIVO, LOJA
         self.codBarras = Label(self.frame_options, text='Cód. De Barras:', font=self.fontepadrao, bg='#f0e68c')
-        self.codBarrasEntry = Entry(self.frame_options, font=self.fontepadrao, bg='#eee8aa')
-        self.codBarrasEntry.place(relx=0.43, rely=0.08, relwidth=0.40, relheight=0.13)
-        self.codBarras.place(relx=0.11, rely=0.08, relwidth=0.30, relheight=0.15)
+        self.store_Exit = Label(self.frame_options, text='Loja:', font=self.fontepadrao, bg='#f0e68c')
+        self.seq_Exit = Label(self.frame_options, text='Sequência:', font=self.fontepadrao, bg='#f0e68c')
+        self.reason_Exit = Label(self.frame_options, text='Motivo:', font=self.fontepadrao, bg='#f0e68c')
+        # ENTRYS
+        self.codBarrasEntry_Exit = Entry(self.frame_options, font=self.fontepadrao, bg='#eee8aa')
+        self.storeEntry_Exit = Entry(self.frame_options, font=self.fontepadrao, bg='#eee8aa')
+        self.seqEntry_Exit = Entry(self.frame_options, font=self.fontepadrao, bg='#eee8aa')
+        self.reasonEntry_Exit = Entry(self.frame_options, font=self.fontepadrao, bg='#eee8aa')
+        # PLACES ENTRYS E LABELS
+        self.codBarrasEntry_Exit.place(relx=0.245, rely=0.08, relwidth=0.25, relheight=0.12)
+        self.storeEntry_Exit.place(relx=0.595, rely=0.08, relwidth=0.08, relheight=0.12)
+        self.seqEntry_Exit.place(relx=0.86, rely=0.08, relwidth=0.11, relheight=0.12)
+        self.reasonEntry_Exit.place(relx=0.45, rely=0.30, relwidth=0.20, relheight=0.12)
+
+        self.codBarras.place(relx=0.02, rely=0.07, relwidth=0.22, relheight=0.15)
+        self.store_Exit.place(relx=0.51, rely=0.07, relwidth=0.08, relheight=0.15)
+        self.seq_Exit.place(relx=0.70, rely=0.07, relwidth=0.14, relheight=0.15)
+        self.reason_Exit.place(relx=0.34, rely=0.29, relwidth=0.10, relheight=0.15)
 
     #  BOTÃO RETIRAR
     def button_Retirar(self):
@@ -302,9 +383,33 @@ class Interface(Funcs):
                               command=self.lens_output)
         self.Retirar["font"] = ("Verdana", 10, "italic", "bold")
         self.Retirar.place(relx=0.05, rely=0.25, relwidth=0.90, relheight=0.05)
-    #<<<<<
 
-    #>>>>>
+    def remove_LensAdd(self):
+        self.verification_IntExit()
+        if self.verification_IntExit():
+            self.captura_dadosRemove()
+            # CHAMANDO BANCO DE DADOS
+            self.connect_BD()
+            self.cursor.execute(f"SELECT amount FROM stock WHERE cod_barras = {self.codRemove_Capt}")
+            result = self.cursor.fetchone()
+            if result is not None:
+                self.cursor.execute(f"UPDATE stock SET amount = amount-1 WHERE cod_barras = {self.codRemove_Capt}")
+                self.cursor.execute(f"INSERT INTO lens VALUES "
+                                    f"('', '{self.date}', '{self.codRemove_Capt}', '{self.store_Capt}', "
+                                    f"'{self.seq_Capt}', '{self.reason_Capt}')")
+                self.conn.commit()
+                self.conn.close()
+                self.text_warning = 'LENTE RETIRADA'
+                self.warning()
+                self.clear_Exit()
+                print('\033[31mINSERIDA NA TABELA DE SAÍDA DE LENTES!\033[m')
+            else:
+                self.text_warning = 'LENTE NÃO EXISTE'
+                self.warning()
+                print('\033[31mNÃO EXISTE A LENTE NO ESTOQUE\033[m')
+    #<<<<<<
+
+    #>>>>>>
     #  OPÇÕES DE REGISTRAR SAÍDAS DE LENTES
     def reg_output(self):
         print("Botão registro de saída clicado!".title())
