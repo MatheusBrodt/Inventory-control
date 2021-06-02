530e73d53ce41e7e34828a4f73d6d64f57568c6d
MatheusBrodt <matheus_brodt@outlook.com>
2021-06-01 21:59:11 -0300

    Criado arquivo EXE.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {'packages': ['os'], 'includes': ['tkinter', 'mysql', 'reportlab']}
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+setup(
+    name='Inventory Control',
+    version='1.0',
+    description='Programa para controle de laboratório de montagem óptica!',
+    options={'build_exe': build_exe_options},
+    executables=[Executable('Versão_Beta1.0.py', base=base)]
+)
